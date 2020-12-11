a428fd55789993615ed88b70dd4fb6759a50ad3c
LIBRA.Bot <LIBRA.Bot>
2020-12-11 12:14:56 -0800

    init release commit

---
+++ wzlib/crypto.go
@@ -0,0 +1,30 @@
+/*
+ * Project Libra cgo usage
+ * Author: A. Yasuda
+ */
+
+package main
+
+import (
+	util "github.com/wzlib/wzutil"
+	"C"
+)
+
+func init(){
+
+	util.Init("agent",true)
+}
+
+//export ValidateToken
+func ValidateToken(om *C.char) (*C.char){
+
+	_om:=C.GoString(om)
+	uid,err:=util.ValidateTokenExp(_om)
+	if err!=nil{
+		util.ErrLog(err)
+		return nil
+	}
+	return C.CString(uid)
+}
+
+func main() {}
