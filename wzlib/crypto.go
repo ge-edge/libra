20165f6cb4b380d6e2ba62bb065127cd47bd002c
安田　アポロ <apolo_yasuda@yahoo.co.jp>
2020-12-11 13:12:30 -0800

    Update crypto.go

--- wzlib/crypto.go
+++ wzlib/crypto.go
@@ -21,7 +21,7 @@ func ValidateToken(om *C.char) (*C.char){
 	_om:=C.GoString(om)
 	uid,err:=util.ValidateTokenExp(_om)
 	if err!=nil{
-		util.ErrLog(err)
+		util.GetCLog().ErrLog(err)
 		return nil
 	}
 	return C.CString(uid)
